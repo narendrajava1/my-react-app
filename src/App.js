cddd29623e9ddf929e6d60ece8a913bf8065d8f2
narendrajava1 <narendrakolli08@gmail.com>
2023-07-04 23:19:21 +0530

    created first component as Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Message } from "./Message";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{advice}</h1>
+      <Message count={count} />
+      <button type="submit" onClick={getAdvice}>
+        Get advice
+      </button>
+      <h1>Welcome to React!!</h1>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+export function Message(countProp) {
+  return (
+    <h3>
+      you have read <strong>{countProp.count} </strong>advices now...
+    </h3>
+  );
+}
